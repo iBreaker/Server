cf7925716920db5ac6a40673293d34f70b0ebb2d
Breaker <791628659@qq.com>
2017-01-02 21:26:04 +0800

    bind sock addr use system call bind()

--- main.c
+++ main.c
@@ -2,6 +2,16 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+
+void server_exit(void)
+{
+    printf("server退出\n");
+    exit(-1);
+}
 
 int main(void)
 {
@@ -9,12 +19,35 @@ int main(void)
     if( -1 == fd )
     {
         printf("get fd error\n");
-
+        server_exit();
     }
     else
     {
         printf("fd is %d\n", fd);
     }
+
+    struct sockaddr_in server_addr;
+    bzero(&server_addr, sizeof(server_addr));
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(0);
+    server_addr.sin_port = htons(9000);
+
+    int bind_result = bind(fd, (struct sockaddr * ) &server_addr, sizeof(server_addr));
+    if(0 == bind_result)
+    {
+        printf("绑定[%s]:%d成功\n", inet_ntoa(server_addr.sin_addr), ntohs(server_addr.sin_port));
+    }
+    else
+    {
+        printf("绑定[%s]:%d失败\n", inet_ntoa(server_addr.sin_addr), ntohs(server_addr.sin_port));
+        printf("错误码%d\n", bind_result);
+        server_exit();
+    }
+
+
+
+
 
     close(fd);
     return 0;
